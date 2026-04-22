4839e3bc3eb79a700b9dd993d403e11b5b1db022
Marlonilao <176732593+Marlonilao@users.noreply.github.com>
2026-04-22 13:00:15 +0800

    refactor: integrate Mantine UI in AboutMe component

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,17 +1,18 @@
-import Togglable from './Togglable'
+import { Accordion, Textarea } from '@mantine/core'
 
 const AboutMe = ({ CVdata, handleInputChange }) => {
   return (
-    <>
-      <Togglable header='About me'>
-        <textarea
+    <Accordion.Item value='aboutMe'>
+      <Accordion.Control>About me</Accordion.Control>
+      <Accordion.Panel>
+        <Textarea
           name='aboutMe'
           value={CVdata.aboutMe}
           onChange={handleInputChange}
           placeholder='Write a brief summary about yourself...'
         />
-      </Togglable>
-    </>
+      </Accordion.Panel>
+    </Accordion.Item>
   )
 }
 
